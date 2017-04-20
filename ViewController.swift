67e70b0185f19287d640ef2f7fd91e6ab35e482f
Alexander Tkachenko <skyalexx@gmail.com>
2017-04-20 11:51:04 +0300

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Alexander Tkachenko on 20/04/17.
+//  Copyright (c) 2017 Alexander Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
